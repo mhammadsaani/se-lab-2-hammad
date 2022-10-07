f21b84a9bc2a9888810ed268da0673437439d4a9
Ramzan Bhatti <mrramzan730@gmail.com>
2022-10-07 22:15:35 +0500

    changes by ramzan

--- lab-02-tasks/slicing.py
+++ lab-02-tasks/slicing.py
@@ -1,3 +1,8 @@
 # String slicing
 namal_uni = "NamalUniversityMianwali"
 print(namal_uni[0:9:2])
+
+# Collaborated by Ramzan(Ramzan)
+print(namal_uni[2:10:3])
+print(namal_uni[1:12:4])
+print(namal_uni[4:8:2])
--- slicing.py
+++
@@ -1,3 +0,0 @@
-# String slicing
-namal_uni = "NamalUniversityMianwali"
-print(namal_uni[0:9:2]
